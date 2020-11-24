a599ce9632cf356b72a778cfcc967963d3c38a82
apptest <apptest@localhost.localdomain>
2020-11-24 00:24:58 -0500

    Prototype updates.

--- ImageGraph.cpp
+++ ImageGraph.cpp
@@ -1,68 +1,129 @@
-#include <stdlib.h>
-#include <vector>
-#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iomanip>
 #include <math.h>
 #include "ImageGraph.h"
 
 using namespace std;
 
-
-ImageGraph::ImageGraph(vector<float> center, vector<vector<float>> points){
-    this->center = center;
-    this->cartesian_points = points;
-    vector<vector<float>> temp (points.size());
-    this->polar_points = temp;
-    this->update_polar();
-}
-
-void ImageGraph::update_polar(){
-    vector<vector<float>> v = cartesian_points;
-
-    for (int i = 0; i < v.size(); i++){
-        vector<float> point = v.at(i);
-        
-        float r = pow(pow(point.at(0), 2) + pow(point.at(0), 2) , 0.5);
-        float theta = atan(point.at(1)/point.at(0));
-        
-        vector<float> new_coordinates = {r, theta};
-        
-        polar_points[i] = new_coordinates;
-    }
-}
-
-void ImageGraph::update_cartesian(){
-    vector<vector<float>> v = polar_points;
-
-    for (int i = 0; i < v.size(); i++){
-        vector<float> point = v.at(i);
-        
-        float x = point.at(0)*cos(point.at(1));
-        float y = point.at(0)*sin(point.at(1));
-        
-        vector<float> new_coordinates = {x,y};
-        
-        cartesian_points[i] = new_coordinates;
-    }
-}
-
-void ImageGraph::dilate(float factor){
-    for (int i = 0; i < this->polar_points.size(); i++){
-        this->polar_points[i][0] *= factor;
-    }
-
-    update_cartesian();
-}
+#define LATKM 110.567
+#define LONGCOEFF 111.320
+#define R_EARTH 6378.1370000
 
 /*
-Checkpoint: Oct 27 2:10pm
-
-Perform vector addition of the converted cartesian points to our new center for below function
-
-Must complete accessors and sting function
+#include <iostream>
+void print(vector<vector<float>> vec){
+    for (int i = 0; i < vec.size(); i++){
+        cout << "[";
+        for (int j = 0; j < vec[i].size(); j++){
+            cout << vec[i][j] << ",";
+        }
+        cout << "]";
+    }
+}
 */
-ImageGraph::ImageGraph ImageGraph::place_on_map(float factor, vector <float> location){
-    center = location;
-    dilate(factor);
 
-    return *this;
+ImageGraph::ImageGraph(vector<vector<float>> pts){
+    cartesian_points = pts;
+    n = cartesian_points.size();
+    recenter();
+    polar_points = to_polar(cartesian_points);
+    max_r = 0;
+    for (auto point : polar_points){max_r = max(point[0], max_r);}
+}
+
+void ImageGraph::recenter(){
+    float x_sum = 0;
+    float y_sum = 0;
+
+    for (auto point : cartesian_points){
+        x_sum += point[0];
+        y_sum += point[1];
+    }
+
+    //compute centroid of image
+    const float centroid_x = x_sum/n;
+    const float centroid_y = y_sum/n;
+    
+    for (int i=0; i<n; i++){
+        cartesian_points[i][0] -= centroid_x; //make x values relative to new origin
+        cartesian_points[i][1] -= centroid_y; //make y values relative to new origin
+    }
+}
+
+vector<vector<float>> ImageGraph::to_polar(vector<vector<float>> cartesian){
+    vector<vector<float>> polar(cartesian.size(), vector<float>(2,0));
+    size_t i = 0;
+    
+    for (auto point : cartesian){
+        float r = sqrt(pow(point[0], 2) + pow(point[1], 2));
+        float theta = atan2(point[1],point[0]);
+        
+        polar[i] = {r, theta};
+        i++;
+    }
+
+    return polar;
+}
+
+vector<vector<float>> ImageGraph::to_cartesian(vector<vector<float>> polar){
+    vector<vector<float>> cartesian(polar.size(), vector<float>(2,0));
+    size_t i = 0;
+
+    for (auto point : polar){
+        float x = point[0]*cos(point[1]);
+        float y = point[0]*sin(point[1]);
+        
+        cartesian[i] = {x,y};
+        i++;
+    }
+
+    return cartesian;
+}
+
+void ImageGraph::set_radius(float r){
+    float factor = r/max_r;
+
+    for (int i = 0; i < n; i++){
+        polar_points[i][0] *= factor;
+    }
+
+    cartesian_points = to_cartesian(polar_points);
+}
+
+void ImageGraph::place_on_map(vector<double> loc, float radius){
+    location = loc;
+    vector<vector<double>> gps(n, vector<double>(2,0));
+
+    for (int i = 0; i<n; i++) {
+        double lat_calc = location[0] + cartesian_points[i][1]/LATKM;
+        double long_calc = location[1] + cartesian_points[i][0]/(LONGCOEFF*cos(lat_calc*M_PI/180.0));
+
+        gps[i] = {lat_calc, long_calc};
+    }
+
+    gps_points = gps;
+}
+
+string ImageGraph::path(){
+    stringstream s;
+    s << fixed << setprecision(6);
+
+    for (auto point : gps_points){
+        s << point[0] << "," << point[1] << "|";
+    }
+
+    return s.str();
+}
+
+vector<vector<float>> ImageGraph::get_cartesian(){
+    return cartesian_points;
+}
+
+vector<vector<float>> ImageGraph::get_polar(){
+    return polar_points;
+}
+
+vector<vector<double>> ImageGraph::get_gps(){
+    return gps_points;
 }
--- test.cpp
+++ test.cpp
@@ -1,22 +1,48 @@
 #include <stdlib.h>
 #include <vector>
 #include <string>
+#include <map>
+#include <iostream>
 #include "ImageGraph.h"
 
 using namespace std;
 
-string test() {
 
-    int n = 4;
-    vector<vector<float>> pic { {-1,-1}, {-1,1}, {1,1}, {1,-1} };
-    vector<float> center = {0,0};
+void test(string tested, string correct) {
+    map<bool ,string> outcome = {{true, "PASSED."},{false, "FAILED."}};
+    bool state = tested == correct;
+    cout << endl << "The following test " << outcome[state] << endl;
+    cout << "Output is: " << tested << endl;
+    cout << "Output should be: " << correct << endl;
+}
 
-    ImageGraph::ImageGraph picture(center, pic); //initialization of object
+int main() {
+    vector<double> toronto = {43.684921, -79.477013};
 
+    vector<vector<float>> pic_A {{0,2},{1,0},{3,0},{4,2},{2,3}}; //irregular pentagon 
+    ImageGraph graph_A(pic_A);
+    graph_A.place_on_map(toronto, 6.4);
+    test(graph_A.path(), "43.690348,-79.501860|43.672259,-79.489433|43.672259,-79.464593|43.690348,-79.452166|43.699392,-79.477013|");
 
-    vector<float> toronto = {43.6532, 79.3832}; //in lat long
-    float r = 1; //in km
-    ImageGraph::ImageGraph mapped = picture.place_on_map(r, toronto);
+    vector<vector<float>> pic_B {{1,1},{-1,1},{0,-1}}; //triangle
+    ImageGraph graph_B(pic_B);
+    graph_B.place_on_map(toronto, 3.24);
+    test(graph_B.path(), "43.690951,-79.464590|43.690951,-79.489436|43.672862,-79.477013|");
 
-    return mapped.get_path();
+    vector<vector<float>> pic_C {{0,0},{0,1},{1,1},{1,0}}; //unit square
+    ImageGraph graph_C(pic_C);
+    graph_C.place_on_map(toronto, 1.6);
+    test(graph_C.path(), "43.680399,-79.483224|43.689443,-79.483225|43.689443,-79.470801|43.680399,-79.470802|");
+
+    vector<vector<float>> pic_D {{1,4},{0,2},{1,0},{4,0},{5,2},{4,4}}; //irregular hexagon
+    ImageGraph graph_D(pic_D);
+    graph_D.place_on_map(toronto, 10.324);
+    test(graph_D.path(), "43.703010,-79.495652|43.684921,-79.508069|43.666832,-79.495641|43.666832,-79.458385|43.684921,-79.445957|43.703010,-79.458374|");
+
+    vector<vector<float>> pic_E {{2,0},{0,2},{-2,0},{0,-2}}; //regular rhombus
+    ImageGraph graph_E(pic_E);
+    graph_E.place_on_map(toronto, 15.4);
+    test(graph_E.path(), "43.684921,-79.452169|43.703010,-79.477013|43.684921,-79.501857|43.666832,-79.477013|");
+
+    return 0;
 }
