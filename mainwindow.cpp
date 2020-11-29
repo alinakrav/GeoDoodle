5da9104c302157bc795d1e07cd4655259a64ebae
kravchenkoalina <alina14022000@gmail.com>
2020-11-28 20:35:53 -0500

    fixed radius of 1 route

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,10 +2,7 @@
 #include "ui_mainwindow.h"
 #include "http.cpp"
 #include "ImageGraph.h"
-#include <vector>
 #include <QString>
-
-vector<double> toronto = {43.684921, -79.477013};
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -34,16 +31,17 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::on_pushButton_3_clicked()
 {
+    radius = ui->spinBox->value();
     ui -> stackedWidget -> setCurrentIndex(3);
 }
 
 void MainWindow::on_cartoon1_clicked()
 {
     ui -> stackedWidget -> setCurrentIndex(4);
-    vector<vector<float>> pic_A {{0,2},{1,0},{3,0},{4,2},{2,3}, {0,2}}; //irregular pentagon
-    ImageGraph graph_A(pic_A);
+    //irregular pentagon
+    ImageGraph graph_A(pentagon);
     graph_A.set_radius(radius);
-    graph_A.place_on_map(toronto, radius);
+    graph_A.place_on_map(toronto);
     graph_A.double_density(2);
     s = QString::fromStdString(graph_A.path());
     Http::urls urls;
@@ -56,10 +54,10 @@ void MainWindow::on_cartoon1_clicked()
 void MainWindow::on_cartoon2_clicked()
 {
     ui -> stackedWidget -> setCurrentIndex(4);
-    vector<vector<float>> pic_B {{1,1},{-1,1},{0,-1}, {1,1}}; //triangle
-    ImageGraph graph_B(pic_B);
+    //triangle
+    ImageGraph graph_B(triangle);
     graph_B.set_radius(radius);
-    graph_B.place_on_map(toronto, radius);
+    graph_B.place_on_map(toronto);
     graph_B.double_density(2);
     s = QString::fromStdString(graph_B.path());
     Http::urls urls;
@@ -72,10 +70,10 @@ void MainWindow::on_cartoon2_clicked()
 void MainWindow::on_cartoon3_clicked()
 {
     ui -> stackedWidget -> setCurrentIndex(4);
-    vector<vector<float>> pic_C {{0,0},{0,1},{1,1},{1,0},{0,0}}; //unit square
-    ImageGraph graph_C(pic_C);
+    // square
+    ImageGraph graph_C(square);
     graph_C.set_radius(radius);
-    graph_C.place_on_map(toronto, radius);
+    graph_C.place_on_map(toronto);
     graph_C.double_density(2);
     s = QString::fromStdString(graph_C.path());
     Http::urls urls;
@@ -89,10 +87,10 @@ void MainWindow::on_cartoon3_clicked()
 void MainWindow::on_cartoon4_clicked()
 {
     ui -> stackedWidget -> setCurrentIndex(4);
-    vector<vector<float>> pic_D {{1,4},{0,2},{1,0},{4,0},{5,2},{4,4},{1,4}}; //irregular hexagon
-    ImageGraph graph_D(pic_D);
+    //irregular hexagon
+    ImageGraph graph_D(hexagon);
     graph_D.set_radius(radius);
-    graph_D.place_on_map(toronto, radius);
+    graph_D.place_on_map(toronto);
     graph_D.double_density(2);
     s = QString::fromStdString(graph_D.path());
     Http::urls urls;
@@ -105,10 +103,10 @@ void MainWindow::on_cartoon4_clicked()
 void MainWindow::on_cartoon5_clicked()
 {
     ui -> stackedWidget -> setCurrentIndex(4);
-    vector<vector<float>> pic_E {{2,0},{0,2},{-2,0},{0,-2},{2,0}}; //regular rhombus
-    ImageGraph graph_E(pic_E);
+    //regular rhombus
+    ImageGraph graph_E(rhombus);
     graph_E.set_radius(radius);
-    graph_E.place_on_map(toronto, radius);
+    graph_E.place_on_map(toronto);
     graph_E.double_density(2);
     s = QString::fromStdString(graph_E.path());
     Http::urls urls;
@@ -119,12 +117,4 @@ void MainWindow::on_cartoon5_clicked()
     ui -> stackedWidget -> setCurrentIndex(5);
 }
 
-void MainWindow::on_stackedWidget_currentChanged(int arg1)
-{
-
-}
-
-void MainWindow::on_spinBox_valueChanged(const int &arg1)
-{
-    radius = arg1;
-}
+void MainWindow::on_stackedWidget_currentChanged(int arg1) {}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vector>
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -28,8 +30,6 @@ private slots:
 
     void on_stackedWidget_currentChanged(int arg1);
 
-    void on_spinBox_valueChanged(const int &arg1);
-
     void on_pushButton_3_clicked();
 
 private:
@@ -37,5 +37,12 @@ private:
     int radius;
     QString s;
     QString url;
+
+    vector<double> toronto = {43.684921, -79.477013};
+    vector<vector<float>> pentagon = {{0,2},{1,0},{3,0},{4,2},{2,3}, {0,2}};
+    vector<vector<float>> triangle {{1,1},{-1,1},{0,-1}, {1,1}};
+    vector<vector<float>> square {{0,0},{0,1},{1,1},{1,0},{0,0}};
+    vector<vector<float>> hexagon {{1,4},{0,2},{1,0},{4,0},{5,2},{4,4},{1,4}};
+    vector<vector<float>> rhombus {{2,0},{0,2},{-2,0},{0,-2},{2,0}};
 };
 #endif // MAINWINDOW_H
